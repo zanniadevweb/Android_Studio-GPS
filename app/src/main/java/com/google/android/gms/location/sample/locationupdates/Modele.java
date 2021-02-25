fcad499544bf9c9980c327a083ad8301cc647620
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2021-02-25 17:41:19 +0100

    Changement du projet de base (vers une localisation en temps réel) et premier tests de récupération de la localisation avec changement de la couleur d'une salle si la salle atteint sa capacité maximum

---
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdates/Modele.java
@@ -0,0 +1,7 @@
+package com.google.android.gms.location.sample.locationupdates;
+
+public class Modele {
+    public static double latitude = 0.0;
+    public static double longitude = 0.0;
+    public static String updatetime = "";
+}
